ca0c36d9f3e1591061955f5a32e87d90301c9fd2
ZahaabAlim <Zahaabalim@gmail.com>
2024-01-05 14:27:33 +0530

    new

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
- region = "us-east-1"
+ region = " ap-south-1"
 }
  
 terraform {
   backend "s3" {
     bucket = "terraform-state-fil"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = " ap-south-1"
  
     dynamodb_table = "TfStateLock"
   }
